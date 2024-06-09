51782f04b371712ea9059b5b5cdd702510bcbbfd
Faahm <fatmahinay99@gmail.com>
2024-06-10 00:52:22 +0800

    display sample current weather

--- src/index.js
+++ src/index.js
@@ -1,11 +1,38 @@
 import "./style.css";
 
-function component() {
-  const element = document.createElement("div");
+const city_title = document.querySelector("[data-city-title]");
+const city_date = document.querySelector("[data-city-date]");
+const current_weather_icon = document.querySelector(
+  "[data-current-weather-icon]"
+);
+const current_condition = document.querySelector("[data-current-condition]");
+const current_temp = document.querySelector("[data-current-temp]");
 
-  element.innerHTML = "uwu";
+const BASE_URL = process.env.BASE_URL;
+const API_KEY = process.env.API_KEY;
 
-  return element;
+async function get_current(city) {
+  const response = await fetch(
+    `${BASE_URL}/current.json?key=${API_KEY}&q=${city}`
+  );
+  const currentCityData = await response.json();
+
+  console.log(`${BASE_URL}/current.json?key=${API_KEY}&q=${city}`);
+
+  currentCityData;
+  display_current_weather(currentCityData);
 }
 
-document.body.appendChild(component());
+function display_current_weather(currentCityData) {
+  city_title.innerText = `${currentCityData["location"]["name"]}`;
+  city_date.innerText = `${currentCityData["location"]["localtime"]}`;
+  current_weather_icon.src = `${currentCityData["current"]["condition"]["icon"]}`;
+  current_condition.innerText = `${currentCityData["current"]["condition"]["text"]}`;
+  current_temp.innerText = `${currentCityData["current"]["temp_c"]}°C`;
+}
+
+// async function get_forecast("London") {
+
+// }
+
+get_current("London");
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const Dotenv = require("dotenv-webpack");
 
 module.exports = {
   mode: "development",
@@ -27,6 +28,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin({
       template: "./src/index.html",
       filename: "index.html",
